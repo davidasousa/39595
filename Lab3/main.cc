3bbbca1a24c5816b0bd383ed6edf90699161dccc
davidasousa <davidasousa335@gmail.com>
2025-03-04 12:59:07 -0500

    Progress

--- Lab3/main.cc
+++ Lab3/main.cc
@@ -180,6 +180,66 @@ int test_part1_4x4_5() {
 	if(sBoard.isValidMove(0, 1, 1, 3)) { return 1; }
 	if(sBoard.isValidMove(0, 1, 2, 0)) { return 1; }
 	if(sBoard.isValidMove(0, 1, 2, 1)) { return 1; }
+	if(sBoard.isValidMove(0, 1, 2, 2)) { return 1; }
+	if(sBoard.isValidMove(0, 1, 2, 3)) { return 1; }
+	if(sBoard.isValidMove(0, 1, 2, 0)) { return 1; }
+	if(sBoard.isValidMove(0, 1, 2, 1)) { return 1; }
+	if(sBoard.isValidMove(0, 1, 2, 2)) { return 1; }
+	if(sBoard.isValidMove(0, 1, 2, 3)) { return 1; }
+
+	// Testing The White Pawn
+	if(sBoard.isValidMove(1, 1, 0, 0)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 0, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 0, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 0, 3)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 1, 0)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 1, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 1, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 1, 3)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 2, 0)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 2, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 2, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 2, 3)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 3, 0)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 3, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 3, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 1, 3, 3)) { return 1; }
+
+	// Testing The White Bishop
+	if(sBoard.isValidMove(2, 2, 0, 0)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 0, 1)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 0, 2)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 0, 3)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 1, 0)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 1, 1)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 1, 2)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 1, 3) == false) { return 1; }
+	if(sBoard.isValidMove(2, 2, 2, 0)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 2, 1)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 2, 2)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 2, 3)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 3, 0)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 3, 1) == false) { return 1; }
+	if(sBoard.isValidMove(2, 2, 3, 2)) { return 1; }
+	if(sBoard.isValidMove(2, 2, 3, 3) == false) { return 1; }
+
+	// Testing The White Rook
+	if(sBoard.isValidMove(1, 0, 0, 0) == false) { return 1; }
+	if(sBoard.isValidMove(1, 0, 0, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 0, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 0, 3)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 1, 0)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 1, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 1, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 1, 3)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 2, 0) == false) { return 1; }
+	if(sBoard.isValidMove(1, 0, 2, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 2, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 2, 3)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 3, 0) == false) { return 1; }
+	if(sBoard.isValidMove(1, 0, 3, 1)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 3, 2)) { return 1; }
+	if(sBoard.isValidMove(1, 0, 3, 3)) { return 1; }
 
 	std::cout << sBoard.displayBoard().str() << std::endl;
 	return 0;
